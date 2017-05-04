18e6c25cf5e69bf0e79106a4256ec764976373c9
leonyh7 <leo_nyh@sina.com>
2017-05-04 18:22:44 +0800

    setting

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -3,98 +3,114 @@
         <Navbar/>
         <main :class="{hideSidebar:!sidebar.opened}">
             <div class="sidebar-wrapper">
-                <Sidebar class="sidebar-container"/>
+                <Sidebar class="sidebar-container" />
             </div>
             <div class="main-container">
                 <App-main/>
+            </div>
+            <div class="apin-setting-detail" v-show="settingShow">
+                <i class="icon-close" @click="hideSetting"></i>
+                <h4 class="apin-setting-title apin-setting-separator">门户设置</h4>
+                <div class="apin-setting-content">
+                    <h6 class="apin-setting-item">
+              设置1
+            </h6>
+                    <div class="apin-setting-color">
+                        <div class="apin-color-option" v-for="(theme, name) in colors" :style="{backgroundColor: theme,width:'30px',height:'30px'}" @click="changeTheme(name)"></div>
+                    </div>
+                </div>
+                <div class="apin-setting-option">
+                    <button class="btn btn-success">确认</button>
+                    <button class="btn btn-default">取消</button>
+                </div>
             </div>
         </main>
     </div>
 </template>
 
 <script>
-    import { Navbar, Sidebar, AppMain } from 'views/layout';
-    import store from 'store';
-    import router from 'router';
-    import permission from 'store/permission';
-    // import { Loading } from 'element-ui';
-    // let loadingInstance;
-    export default {
-      name: 'layout',
-      components: {
+import { Navbar, Sidebar, AppMain } from 'views/layout';
+import store from 'store';
+import router from 'router';
+import permission from 'store/permission';
+// import { Loading } from 'element-ui';
+// let loadingInstance;
+export default {
+    name: 'layout',
+    components: {
         Navbar,
         Sidebar,
         AppMain
-      },
-      computed: {
+    },
+    computed: {
         sidebar() {
-          return this.$store.state.app.sidebar;
+            return this.$store.state.app.sidebar;
         }
-      },
-      beforeRouteEnter: (to, from, next) => {
+    },
+    beforeRouteEnter: (to, from, next) => {
         const roles = store.getters.roles;
         if (roles.length !== 0) {
-          next();
-          return
+            next();
+            return
         }
 
         // loadingInstance = Loading.service({ fullscreen: true, text: '玩命加载中' });
         store.dispatch('GetInfo').then(() => {
-          permission.init({
-            roles: store.getters.roles,
-            router: router.options.routes
-          });
-        //   loadingInstance.close();
-          next();
+            permission.init({
+                roles: store.getters.roles,
+                router: router.options.routes
+            });
+            //   loadingInstance.close();
+            next();
         }).catch(err => {
-        //   loadingInstance.close();
-          console.log(err);
+            //   loadingInstance.close();
+            console.log(err);
         });
-      }
     }
+}
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
-    @import "src/styles/mixin.scss";
-    .app-wrapper {
-        @include clearfix;
-        position: relative;
-        height: 100%;
-        width: 100%;
+@import "src/styles/mixin.scss";
+.app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    main {
         display: flex;
-        flex-direction: column;
-        main {
-            display: flex;
-            flex: 1;
-            &.hideSidebar {
-                .sidebar-wrapper {
-                    transform: translate(-140px, 0);
+        flex: 1;
+        &.hideSidebar {
+            .sidebar-wrapper {
+                transform: translate(-140px, 0);
+                .sidebar-container {
+                    transform: translate(132px, 0);
+                }
+                &:hover {
+                    transform: translate(0, 0);
                     .sidebar-container {
-                        transform: translate(132px, 0);
-                    }
-                    &:hover {
                         transform: translate(0, 0);
-                        .sidebar-container {
-                            transform: translate(0, 0);
-                        }
                     }
                 }
             }
         }
-        .sidebar-wrapper {
-            width: 180px;
-            overflow-x: hidden;
-            overflow-y: auto;
-            transition: all .28s ease-out;
-            @include scrollBar;
-        }
-        .sidebar-container {
-            transition: all .28s ease-out;
-        }
-        .main-container {
-            flex: 1;
-            overflow-y: auto;
-            transition: all .28s ease-out;
-            @include scrollBar;
-        }
     }
+    .sidebar-wrapper {
+        width: 180px;
+        overflow-x: hidden;
+        overflow-y: auto;
+        transition: all .28s ease-out;
+        @include scrollBar;
+    }
+    .sidebar-container {
+        transition: all .28s ease-out;
+    }
+    .main-container {
+        flex: 1;
+        overflow-y: auto;
+        transition: all .28s ease-out;
+        @include scrollBar;
+    }
+}
 </style>
